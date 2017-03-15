bfe1fdc7c92f76a6cc1faa990aefcab076123d22
Ksis <legends.98@gmail.com>
2017-03-15 16:37:12 +0100

    Aded Hello_world program

---
+++ src/hello_world.cpp
@@ -0,0 +1,11 @@
+#include<ros/ros.h>
+#include<iostream>
+int main(int argc, char** argv)
+{
+  //ros::init(argc,argv,"bye_world");
+  //ros::NodeHandle nh;
+
+std::cout<<"Hello_World"<< std::endl;
+//ros::spin();
+return 0;
+         }
